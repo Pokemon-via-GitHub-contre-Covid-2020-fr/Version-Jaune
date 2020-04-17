29b186670711e7037cbbb26ddc516c7950acdc58
Lilian Besson <lilian.besson@crans.org>
2020-04-17 18:58:47 +0200

    Cf #14

--- zenity_script_to_enter_summary.sh
+++ zenity_script_to_enter_summary.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
+#
+# Script bash pour appeler (si disponible) "zenity --entry" pour éditer le message de commit.
+#
+# Ces deux lignes vides sont là pour faire un joli message de commit, comme cela :
+#
+# DONE lilian termine, auto commit avec 'make commit_done'
+#
+# J'ai entraîné l'équipe, battu le rival, et on est dans l'arène de Pierre
 
 echo -e "\n\n"
-zenity --entry \
+
+type zenity && { zenity --entry \
        --timeout=60 --width=640 --height=440 \
        --title="Pokémon via GitHub contre Covid-19" \
        --entry-text="J'ai capturé tel Pokémon, j'ai battu tel dresseur, j'ai découvert tel endroit..." \
-       --text="Raconte ta session de jeu en quelques lignes maximum :"
+       --text="Raconte ta session de jeu en quelques lignes maximum :" \
+; } || echo "Zenity pas trouvé. Regarde https://github.com/Pokemon-via-GitHub-contre-Covid-2020-fr/Version-Jaune/issues/14 ?"
--- zenity_script_to_validate_commit.sh
+++ zenity_script_to_validate_commit.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
+#
+# Script bash pour appeler (si disponible) "zenity --question" pour valider le commit.
+#
 
-zenity --question \
+type zenity && { zenity --question \
        --timeout=60 --width=640 --height=440 \
        --title="Pokémon via GitHub contre Covid-19" \
        --text="$(echo -e "Voulez-vous vraiment commiter votre sauvegarde ?\n\n$(ls -larth Pokemon_Yellow_FRENCH_GBC-HS.sav)")"
+; } || echo "Zenity pas trouvé. Regarde https://github.com/Pokemon-via-GitHub-contre-Covid-2020-fr/Version-Jaune/issues/14 ?"
