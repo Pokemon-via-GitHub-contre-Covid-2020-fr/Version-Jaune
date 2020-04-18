a7974b53294094e471c9ed7d457c0913464441b2
Lilian Besson <lilian.besson@crans.org>
2020-04-18 18:53:12 +0200

    START lilian commence, auto commit avec 'make commit_start'

--- update_readme_start.sh
+++ update_readme_start.sh
@@ -11,8 +11,11 @@ fi
 echo "$author"
 
 sed -i.backup s/"^.*joue actuellement."/"$author"/ README.md
-#diff README.md README.md.backup
+
+# diff README.md README.md.backup  # DEBUG
 git diff README.md README.md.backup
-#echo "OK pour supprimer le fichier README.md.backup et valider ce changement ?"
-#read
-#trash README.md.backup
+
+# echo "OK pour supprimer le fichier README.md.backup et valider ce changement ?"  # DEBUG
+# read  # DEBUG
+
+# trash README.md.backup  # DEBUG
--- zenity_script_to_validate_end.sh
+++ zenity_script_to_validate_end.sh
@@ -3,8 +3,9 @@
 # Script bash pour appeler (si disponible) "zenity --question" pour valider le début de jeu.
 #
 
-type zenity > /dev/null && { zenity --question \
-       --timeout=60 --width=640 --height=440 \
+type zenity >/dev/null && { \
+zenity --question \
+       --timeout=60 --width=440 --height=200 \
        --title="Pokémon via GitHub contre Covid-19" \
-       --text="$(echo -e "Voulez-vous vraiment continuer le jeu depuis cette sauvegarde ?\n\n$(ls -larth Pokemon_Yellow_FRENCH_GBC-HS.sav)")" \
+       --text="$(echo -e "Voulez-vous vraiment commiter votre sauvegarde ?\n\n$(file Pokemon_Yellow_FRENCH_GBC-HS.sav)\n$(ls -larth Pokemon_Yellow_FRENCH_GBC-HS.sav)")" \
 ; } || echo "Zenity pas trouvé. Regarde https://github.com/Pokemon-via-GitHub-contre-Covid-2020-fr/Version-Jaune/issues/14 ?"
